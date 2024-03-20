ba1dbd766063483a8ebf2ef0edd7c6ad085c0f39
talamit999 <102660057+talamit999@users.noreply.github.com>
2024-03-20 10:45:19 +0200

    CXX_CALCULATION

--- evaluate.py
+++ evaluate.py
@@ -6,6 +6,9 @@ import datasets
 from torch.autograd import Variable
 import torch.nn.functional as F
 
+import numpy as np
+import pandas as pd
+
 def test_outage(model, test_loader, num_devices, outages):
     if torch.cuda.is_available():
         device = torch.device("cuda")
@@ -15,19 +18,42 @@ def test_outage(model, test_loader, num_devices, outages):
 
     model.eval()
     num_correct = 0
-    for data, target in tqdm(test_loader, leave=False):
+    ##flag = 0
+    r = []
+    for data, target in tqdm(test_loader, leave=False): #target dimentions: [32], where 32 is the batch size
         for outage in outages:
             data[:, outage] = 0
         data, target = data.to(), target.to()
         data, target = Variable(data), Variable(target)
-        predictions = model(data)
-        cloud_pred = predictions[-1]
+        predictions, z = model(data)
+        #r.append(z)
+        cloud_pred = predictions[-1] # dimentions: [32,10] represent the 32 samples, where to each one there's an array of probability(?) to be in the i-th class
         loss = F.cross_entropy(cloud_pred, target, size_average=False).item()
-        pred = cloud_pred.data.max(1, keepdim=True)[1]
+
+        pred = cloud_pred.data.max(1, keepdim=True)[1] #dimentions: [32, 1] represent the class chosen by the cloud with max probabilty
+        '''if flag < 1:
+            print(cloud_pred)
+            print(pred)
+        flag = flag + 1'''
         correct = (pred.view(-1) == target.view(-1)).long().sum().item()
         num_correct += correct
 
-    N = len(test_loader.dataset)
+    ##########################################################################################################333
+    '''g = np.concatenate(r, axis=0) #(9984, 96, 14, 9)
+    c = g.reshape(-1, g.shape[-1])
+
+    # Convert the matrix to a Pandas DataFrame
+    df = pd.DataFrame(c)
+
+    # Specify the CSV file path
+    csv_file_path = 'output_matrix.csv'
+
+    # Write the DataFrame to a CSV file
+    df.to_csv(csv_file_path, index=False, header=False)
+
+    cov_matrix = c @ c.T
+    print(cov_matrix.shape)'''
+    N = len(test_loader.dataset) # N = 10,000, number of iterations in the above loop: 312 (which is N/batch_size)
 
     return 100. * (num_correct / N)
 
@@ -55,6 +81,7 @@ if __name__ == '__main__':
     num_devices = x.shape[1]
     in_channels = x.shape[2]
     model = torch.load(args.model_path, map_location=torch.device('cpu'))
+    '''
     for i in range(num_devices):
         outages = [i]
         acc = test_outage(model, test_loader, num_devices, outages)
@@ -64,3 +91,8 @@ if __name__ == '__main__':
         outages = list(range(i, num_devices))
         acc = test_outage(model, test_loader, num_devices, outages)
         print('Missing Device(s) {}: {:.4f}'.format(outages, acc))
+    '''
+
+    outages = []
+    acc = test_outage(model, test_loader, num_devices, outages)
+    print('Missing Device(s) {}: {:.4f}'.format(outages, acc))
--- net.py
+++ net.py
@@ -4,6 +4,7 @@ from __future__ import print_function
 import torch
 import torch.nn as nn
 
+import numpy as np
 
 def _layer(in_channels, out_channels, activation=True):
     if activation:
@@ -78,130 +79,50 @@ class DDNN(nn.Module):
             ResLayer(128, 128),
         )
         self.pool = nn.AdaptiveAvgPool2d(1)
-        self.classifier = nn.Linear(128, out_channels)
+        self.classifier = nn.Linear(128, out_channels) #out_channels = 10
 
 
     def forward(self, x):
+    ###################################################################################
         mu = [0, 0, 0, 0, 0, 0]                          # mean of the distribution,dimentions :[num_devices=6, 1]
         std_dev = [1, 1, 1, 1, 1, 1]                     # square of variance of noise per device,dimentions :[num_devices=6, 1]
+        ##self.flag = 0
 
-        B = x.shape[0]
+        B = x.shape[0]                                   #get the first dimention of x, meaning: 32 (batch size)
         hs, predictions = [], []
+        z_list = []
         for i, device_model in enumerate(self.device_models):
+            h, prediction = device_model(x[:, i])         # h is the device exit data, dimentions :[32, 16, 14, 9]
 
-            size = (B, 16, 14, 9)                         # size of the array or shape of the noise
-            noise = torch.normal(mu[i], std_dev[i], size) # create a tensor with gausian noises
+            #z = np.concatenate((z.detach().numpy(), h.detach().numpy()), axis=1)
+            #noise = torch.normal(mu[i], std_dev[i], h.size())  # create a tensor with gausian noises at the same dimentions as h                                           # prediction is the prediction per device, dimentions: [32, 10]
+            #h = h + noise                                 # adding the noise to the device output data
 
-            h, prediction = device_model(x[:, i])         # h is the device exit data, dimentions :[32, 16, 24, 9]
-                                                          # prediction is the prediction per device, dimentions: [32, 10]
-            h = h + noise                                 # adding the noise to the device output data
+            # Convert h to NumPy array
+            h_np = h.detach().numpy()
+
+            # Append h_np to the list
+            z_list.append(h_np)
+
+
             hs.append(h)
             predictions.append(prediction)
 
-        # TO ADD GAUSIAN NOISE IF SIGMA IS CONST PER DEVICE
+        z = np.concatenate(z_list, axis=1) # output of all 6 devices for a batch
 
-        h = torch.cat(hs, dim=1)
-        h = self.cloud_model(h)
-        h = self.pool(h)
-        prediction = self.classifier(h.view(B, -1))
-        predictions.append(prediction)                     # add the cloud prediction as the last item of the array
-        return predictions
+        z_mat = z.reshape(-1, z.shape[-1]) #2D
 
-'''
+        z_cov_matrix = z_mat @ z_mat.T
+        z_trace = np.trace(z_cov_matrix)
+        print(z_trace)
+    ##########################################################################################3
 
-from __future__ import print_function
-import torch
+        h = torch.cat(hs, dim=1) # concatenate the itens in hs to h, dimentions: [32, 96, 14, 9]
+        h = self.cloud_model(h)  # dimentions: [32, 128, 7, 4]
+        h = self.pool(h) # dimentions: [32, 128, 1, 1]
+        prediction = self.classifier(h.view(B, -1)) # dimentions: [32,10] ; h.view(B, -1) size: [32, 128]
+        predictions.append(prediction)              # add the cloud prediction as the last item of the array
 
-import torch.nn as nn
-
-
-def _layer(in_channels, out_channels, activation=True):
-    if activation:
-        return nn.Sequential(
-            nn.Conv2d(in_channels, out_channels, 3, stride=1, padding=1),
-            nn.BatchNorm2d(out_channels),
-            nn.ReLU(inplace=True)
-        )
-    else:
-        return nn.Sequential(
-            nn.Conv2d(in_channels, out_channels, 3, stride=1, padding=1),
-            nn.BatchNorm2d(out_channels),
-        )
+        return predictions, z
 
 
-class ResLayer(nn.Module):
-    def __init__(self, in_channels, out_channels):
-        super(ResLayer, self).__init__()
-        self.c1 = _layer(in_channels, out_channels)
-        self.c2 = _layer(out_channels, out_channels, activation=False)
-        self.activation = nn.ReLU(inplace=True)
-
-    def forward(self, x):
-        h = self.c1(x)
-        h = self.c2(h)
-
-        # residual connection
-        if x.shape[1] == h.shape[1]:
-            h += x
-
-        h = self.activation(h)
-
-        return h
-
-
-class DeviceModel(nn.Module):
-    def __init__(self, in_channels, out_channels):
-        super(DeviceModel, self).__init__()
-        self.model = nn.Sequential(
-            ResLayer(in_channels, 8),
-            ResLayer(8, 8),
-            ResLayer(8, 16),
-            ResLayer(16, 16),
-            ResLayer(16, 16),
-        )
-        self.pool = nn.AdaptiveAvgPool2d(1)
-        self.classifier = nn.Linear(16, out_channels)
-
-    def forward(self, x):
-        B = x.shape[0]
-        h = self.model(x)
-        p = self.pool(h)
-        return h, self.classifier(p.view(B, -1))
-
-
-class DDNN(nn.Module):
-    def __init__(self, in_channels, out_channels, num_devices):
-        super(DDNN, self).__init__()
-        self.num_devices = num_devices
-        self.device_models = []
-        for _ in range(num_devices):
-            self.device_models.append(DeviceModel(in_channels, out_channels))
-        self.device_models = nn.ModuleList(self.device_models)
-
-        cloud_input_channels = 16 * num_devices
-        self.cloud_model = nn.Sequential(
-            ResLayer(cloud_input_channels, 64),
-            ResLayer(64, 64),
-            ResLayer(64, 128),
-            nn.AvgPool2d(2, 2),
-            ResLayer(128, 128),
-            ResLayer(128, 128),
-        )
-        self.pool = nn.AdaptiveAvgPool2d(1)
-        self.classifier = nn.Linear(128, out_channels)
-
-    def forward(self, x):
-        B = x.shape[0]
-        hs, predictions = [], []
-        for i, device_model in enumerate(self.device_models):
-            h, prediction = device_model(x[:, i])
-            hs.append(h)
-            predictions.append(prediction)
-
-        h = torch.cat(hs, dim=1)
-        h = self.cloud_model(h)
-        h = self.pool(h)
-        prediction = self.classifier(h.view(B, -1))
-        predictions.append(prediction)
-        return predictions
-'''
